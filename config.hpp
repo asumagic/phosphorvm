7853109c98170418518f7423606e04d4c3ab75d9
AsuMagic <sdelang@sdelang.fr>
2018-11-11 15:25:47 +0100

    Reduce max frame stack size for now

--- config.hpp
+++ config.hpp
@@ -6,4 +6,4 @@ constexpr bool debug_mode = true;
 
 constexpr std::size_t max_stack_depth = 1024 * 32; // 32KiB
 constexpr std::size_t max_context_depth = 256;
-constexpr std::size_t max_call_depth = 256;
+constexpr std::size_t max_call_depth = 32;
