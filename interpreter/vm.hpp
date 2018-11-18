0792449db86a12e753f0ade4a95702e4263c2c06
AsuMagic <sdelang@sdelang.fr>
2018-11-18 17:23:21 +0100

    Move vminline.hpp contents to vm.hpp as it's no longer necessary

--- interpreter/vm.hpp
+++ interpreter/vm.hpp
@@ -3,12 +3,21 @@
 #include "contextstack.hpp"
 #include "framestack.hpp"
 #include "mainstack.hpp"
+#include "traits/variable.hpp"
 #include "variablereference.hpp"
 #include "../config.hpp"
 #include "../unpack/chunk/form.hpp"
 #include "../util/compilersupport.hpp"
 #include "../util/errormanagement.hpp"
 #include "../bytecode/types.hpp"
+
+#define DISPATCH_NEXT(appended_type) \
+	dispatcher< \
+		Left - 1, \
+		F, \
+		Ts..., \
+		appended_type \
+	>(f, new_array);
 
 template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
@@ -53,4 +62,83 @@ public:
 	void execute(const Script& script);
 };
 
-#include "vminline.hpp"
+inline VM::VM(const Form& p_form) :
+	form{p_form}
+{
+	if constexpr (check(debug::vm_debug_stack))
+	{
+		std::fill(stack.raw.begin(), stack.raw.end(), 0xAB);
+	}
+}
+
+template<std::size_t Left, class F, class... Ts>
+FORCE_INLINE
+void VM::dispatcher(F f, [[maybe_unused]] std::array<DataType, Left> types)
+{
+	if constexpr (Left == 0)
+	{
+		f(Ts{}...);
+	}
+	else
+	{
+		std::array<DataType, Left - 1> new_array;
+		std::copy(types.begin() + 1, types.end(), new_array.begin());
+
+		switch(types.front())
+		{
+		case DataType::f64: DISPATCH_NEXT(f64) break;
+		case DataType::f32: DISPATCH_NEXT(f32) break;
+		case DataType::i64: DISPATCH_NEXT(s64) break;
+		case DataType::i32: DISPATCH_NEXT(s32) break;
+		case DataType::i16: DISPATCH_NEXT(s16) break;
+		case DataType::str: DISPATCH_NEXT(StringReference) break;
+		case DataType::var: DISPATCH_NEXT(VariablePlaceholder) break;
+		default: maybe_unreachable();
+		}
+	}
+}
+
+template<class T>
+FORCE_INLINE
+void VM::push_stack_variable(const T& value)
+{
+	auto padding_bytes = sizeof(s64) - sizeof(T);
+
+	stack.push(value);
+	stack.skip(-padding_bytes);
+	stack.push(data_type_for<T>::value);
+}
+
+template<class T>
+FORCE_INLINE
+VariableReference<T> VM::read_variable_reference(InstType inst_type, VarId var_id)
+{
+	VariableReference<T> ret{inst_type, var_id};
+
+	switch (inst_type)
+	{
+	case InstType::stack_top_or_global:
+		stack.skip(sizeof(s64) - sizeof(T));
+		ret.set_read_value(stack.pop<T>());
+		break;
+
+	default:
+		maybe_unreachable("Unimplemented read_variable_reference for InstType");
+	}
+
+	return ret;
+}
+
+template<class T>
+FORCE_INLINE
+auto VM::value(T& value)
+{
+	if constexpr (is_var<T>())
+	{
+		return value.get_read_value();
+	}
+	else
+	{
+		return value;
+	}
+}
