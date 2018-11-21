ed17cf68f56667101fb9e4167e54f28b5f1c122b
AsuMagic <sdelang@sdelang.fr>
2018-11-21 19:46:10 +0100

    Refactor disassembly to be able to disassemble individual lines

--- bytecode/disasm.cpp
+++ bytecode/disasm.cpp
@@ -5,6 +5,11 @@
 #include <fmt/color.h>
 #include <string_view>
 
+std::string DisassembledInstruction::as_plain_string() const
+{
+	return fmt::format("{}{:15}{:30} ; {}", location, mnemonic, params, comment);
+}
+
 std::string Disassembler::get_string(s32 id)
 {
 	if (id < 0 || size_t(id) >= _form.strg.elements.size())
@@ -93,192 +98,186 @@ std::string Disassembler::comparator_name(u8 function)
 	return "<bad>";
 }
 
+DisassembledInstruction Disassembler::disassemble_block(const Block* block, const Script* script)
+{
+	DisassembledInstruction disasm;
+	disasm.begin_block = block;
+
+	const Block* main_block_ptr = block++;
+	auto main_block = *main_block_ptr;
+
+	auto op = (main_block >> 24) & 0xFF;
+
+	// Type pairs
+	auto t1 = (main_block >> 16) & 0xF;
+	auto t2 = (main_block >> 20) & 0xF;
+
+	if (script != nullptr)
+	{
+		disasm.location = fmt::format(
+			"${:08x}: ",
+			std::distance(script->data.data(), main_block_ptr)
+		);
+	}
+
+	auto read_block_operand = [&](auto& into) {
+		std::memcpy(&into, block, sizeof(decltype(into)));
+		block += sizeof(std::decay_t<decltype(into)>) / sizeof(Block);
+		return into;
+	};
+
+	auto push_param = [&](auto type) -> std::string {
+		switch (DataType(type))
+		{
+		case DataType::f64: { f64 v; return fmt::to_string(read_block_operand(v)); }
+		case DataType::f32: { f32 v; return fmt::to_string(read_block_operand(v)); }
+		case DataType::i32: { s32 v; return fmt::to_string(read_block_operand(v)); }
+		case DataType::i64: { s64 v; return fmt::to_string(read_block_operand(v)); }
+		case DataType::b32: { s32 v; return fmt::to_string(bool(read_block_operand(v))); }
+		case DataType::var: { u32 v; return fmt::to_string(resolve_variable_name(read_block_operand(v) & 0xFFFFFF)); }
+		case DataType::str: { s32 v; return fmt::format("\"{}\"", get_string(read_block_operand(v))); }
+		case DataType::i16: return fmt::to_string(main_block & 0xffff);
+		}
+
+		return "<unknown>";
+	};
+
+	auto generic_1t = [&](auto name) {
+		disasm.mnemonic = fmt::format("{}.{}", name, type_suffix(t1));
+	};
+
+	auto generic_2t = [&](auto name) {
+		disasm.mnemonic = fmt::format("{}.{}.{}", name, type_suffix(t1), type_suffix(t2));
+	};
+
+	auto generic_push = [&](auto name, auto type) {
+		disasm.mnemonic = fmt::format("{}.{}", name, type_suffix(type));
+		disasm.params = push_param(t1);
+	};
+
+	// TODO: add labels for easier disassembly read
+	auto generic_goto = [&](auto name) {
+		disasm.mnemonic = name;
+	};
+
+	switch (Instr(op))
+	{
+	case Instr::opconv:     generic_2t("conv"); break;
+	case Instr::opmul:      generic_2t("mul"); break;
+	case Instr::opdiv:      generic_2t("div"); break;
+	case Instr::oprem:      generic_2t("rem"); break;
+	case Instr::opmod:      generic_2t("mod"); break;
+	case Instr::opadd:      generic_2t("add"); break;
+	case Instr::opsub:      generic_2t("sub"); break;
+	case Instr::opand:      generic_2t("and"); break;
+	case Instr::opor:       generic_2t("or"); break;
+	case Instr::opxor:      generic_2t("xor"); break;
+	case Instr::opshl:      generic_2t("shl"); break;
+	case Instr::opshr:      generic_2t("shr"); break;
+
+	case Instr::opneg:      generic_1t("neg"); break;
+	case Instr::opnot:      generic_1t("not"); break;
+	case Instr::opret:      generic_1t("ret"); break;
+	case Instr::oppopz:     generic_1t("popz"); break;
+	case Instr::opdup:      generic_1t("dup"); break;
+
+	case Instr::opexit:     disasm.mnemonic = "exit"; break;
+
+	case Instr::opb:        generic_goto("b"); break;
+	case Instr::opbt:       generic_goto("bt"); break;
+	case Instr::opbf:       generic_goto("bf"); break;
+	case Instr::oppushenv:  generic_goto("pushenv"); break;
+	case Instr::oppopenv:   generic_goto("popenv"); break;
+
+	case Instr::oppushcst:  generic_push("pushcst", t1); break;
+	case Instr::oppushloc:  generic_push("pushloc", t1); break;
+	case Instr::oppushglb:  generic_push("pushglb", t1); break;
+
+	case Instr::oppushspc: {
+		disasm.mnemonic = "pushspc";
+		disasm.params = "<bad>";
+
+		auto reference_block = *(block++);
+
+		auto& defs = _form.vari.definitions;
+		auto it = std::find_if(defs.begin(), defs.end(), [&](auto& def) {
+			return (reference_block & 0x00FFFFFF) == s32(def.special_var);
+		});
+
+		if (it != defs.end())
+		{
+			disasm.params = it->name;
+		}
+		else
+		{
+			// HACK: move back to the older block_ptr then read it as a
+			// regular variable name if reading the special var fails
+
+			--block;
+			disasm.params = fmt::format("<unimpl:{}>", push_param(t1));
+		}
+	} break;
+
+	case Instr::oppushi16: {
+		disasm.mnemonic = "push.i16";
+		disasm.params = fmt::to_string(main_block & 0xFFFF);
+	} break;
+
+	case Instr::opcmp: {
+		disasm.mnemonic = fmt::format("cmp.{}.{}", type_suffix(t1), type_suffix(t2));
+		disasm.params = comparator_name(u8((main_block >> 8) & 0xFF));
+	} break;
+
+		// TODO: pop.i16.var seems to cause issues
+	case Instr::oppop: {
+		disasm.mnemonic = fmt::format("popv.{}", type_suffix(t2));
+		disasm.params = resolve_variable_name(*(block++));
+	} break;
+
+	case Instr::opcall: {
+		disasm.mnemonic = fmt::format("call.{}", type_suffix(t1));
+		disasm.params = resolve_function_name(*(block++));
+	} break;
+
+	case Instr::opbreak: {
+		disasm.mnemonic = "break";
+		disasm.params = fmt::to_string(static_cast<s16>(main_block & 0xFFFF));
+	} break;
+
+	default: {
+		disasm.mnemonic = "<bad>";
+		disasm.comment = "!!! This may indicate corruption";
+	}
+
+	};
+
+	disasm.comment.insert(0, fmt::format(
+		"${:08x} ",
+		fmt::join(std::vector<Block>(main_block_ptr, block), "'")
+	));
+
+	disasm.end_block = block;
+
+	return disasm;
+}
+
 void Disassembler::operator()(const Script& script)
 {
 	auto& program = script.data;
 
 	fmt::print(fmt::color::orange, "\nDisassembly of '{}': {} blocks ({} bytes)\n", script.name, program.size(), program.size() * 4);
 
-	_block_ptr = script.data.data();
+	const Block* block = script.data.data();
 
-	while (_block_ptr != &(*script.data.end()))
+	while (block != &(*script.data.end()))
 	{
-		const Block* main_block_ptr = _block_ptr++;
-		auto main_block = *main_block_ptr;
+		auto disasm = disassemble_block(block, &script);
 
-		auto op = (main_block >> 24) & 0xFF;
+		fmt::print(fmt::color::gray, "{}", disasm.location);
+		fmt::print("{:15}", disasm.mnemonic);
+		fmt::print("{:30}", disasm.params);
+		fmt::print(fmt::color::gray, " ; {}\n", disasm.comment);
 
-		// Type pairs
-		auto t1 = (main_block >> 16) & 0xF;
-		auto t2 = (main_block >> 20) & 0xF;
-
-		fmt::print(
-			fmt::color::light_gray,
-			"${:08x}: ",
-			std::distance(script.data.data(), main_block_ptr),
-			main_block
-		);
-
-		std::string mnemonic = "<unimpl>", params, comment;
-
-		auto read_block_operand = [&](auto& into) {
-			std::memcpy(&into, _block_ptr, sizeof(decltype(into)));
-			_block_ptr += sizeof(std::decay_t<decltype(into)>) / sizeof(Block);
-			return into;
-		};
-
-		auto push_param = [&](auto type) -> std::string {
-			switch (DataType(type))
-			{
-			case DataType::f64: { f64 v; return fmt::to_string(read_block_operand(v)); }
-			case DataType::f32: { f32 v; return fmt::to_string(read_block_operand(v)); }
-			case DataType::i32: { s32 v; return fmt::to_string(read_block_operand(v)); }
-			case DataType::i64: { s64 v; return fmt::to_string(read_block_operand(v)); }
-			case DataType::b32: { s32 v; return fmt::to_string(bool(read_block_operand(v))); }
-			case DataType::var: { u32 v; return fmt::to_string(resolve_variable_name(read_block_operand(v) & 0xFFFFFF)); }
-			case DataType::str: { s32 v; return fmt::format("\"{}\"", get_string(read_block_operand(v))); }
-			case DataType::i16: return fmt::to_string(main_block & 0xffff);
-			}
-
-			return "<unknown>";
-		};
-
-		auto generic_1t = [&](auto name) {
-			mnemonic = fmt::format("{}.{}", name, type_suffix(t1));
-		};
-
-		auto generic_2t = [&](auto name) {
-			mnemonic = fmt::format("{}.{}.{}", name, type_suffix(t1), type_suffix(t2));
-		};
-
-		auto generic_push = [&](auto name, auto type) {
-			mnemonic = fmt::format("{}.{}", name, type_suffix(type));
-			params = push_param(t1);
-		};
-
-		// TODO: add labels for easier disassembly read
-		auto generic_goto = [&](auto name) {
-			mnemonic = name;
-		};
-
-		switch (Instr(op))
-		{
-		case Instr::opconv:     generic_2t("conv"); break;
-		case Instr::opmul:      generic_2t("mul"); break;
-		case Instr::opdiv:      generic_2t("div"); break;
-		case Instr::oprem:      generic_2t("rem"); break;
-		case Instr::opmod:      generic_2t("mod"); break;
-		case Instr::opadd:      generic_2t("add"); break;
-		case Instr::opsub:      generic_2t("sub"); break;
-		case Instr::opand:      generic_2t("and"); break;
-		case Instr::opor:       generic_2t("or"); break;
-		case Instr::opxor:      generic_2t("xor"); break;
-		case Instr::opshl:      generic_2t("shl"); break;
-		case Instr::opshr:      generic_2t("shr"); break;
-
-		case Instr::opneg:      generic_1t("neg"); break;
-		case Instr::opnot:      generic_1t("not"); break;
-		case Instr::opret:      generic_1t("ret"); break;
-		case Instr::oppopz:     generic_1t("popz"); break;
-		case Instr::opdup:      generic_1t("dup"); break;
-
-		case Instr::opexit:     mnemonic = "exit"; break;
-
-		case Instr::opb:        generic_goto("b"); break;
-		case Instr::opbt:       generic_goto("bt"); break;
-		case Instr::opbf:       generic_goto("bf"); break;
-		case Instr::oppushenv:  generic_goto("pushenv"); break;
-		case Instr::oppopenv:   generic_goto("popenv"); break;
-
-		case Instr::oppushcst:  generic_push("pushcst", t1); break;
-		case Instr::oppushloc:  generic_push("pushloc", t1); break;
-		case Instr::oppushglb:  generic_push("pushglb", t1); break;
-
-		case Instr::oppushspc: {
-			mnemonic = "pushspc";
-			params = "<bad>";
-
-			auto reference_block = *(_block_ptr++);
-
-			auto& defs = _form.vari.definitions;
-			auto it = std::find_if(defs.begin(), defs.end(), [&](auto& def) {
-				return (reference_block & 0x00FFFFFF) == s32(def.special_var);
-			});
-
-			if (it != defs.end())
-			{
-				params = it->name;
-			}
-			else
-			{
-				// HACK: move back to the older block_ptr then read it as a
-				// regular variable name if reading the special var fails
-
-				--_block_ptr;
-				params = fmt::format("<unimpl:{}>", push_param(t1));
-			}
-		} break;
-
-		case Instr::oppushi16: {
-			mnemonic = "push.i16";
-			params = fmt::to_string(main_block & 0xFFFF);
-		} break;
-
-		case Instr::opcmp: {
-			mnemonic = fmt::format("cmp.{}.{}", type_suffix(t1), type_suffix(t2));
-			params = comparator_name(u8((main_block >> 8) & 0xFF));
-		} break;
-
-		// TODO: pop.i16.var seems to cause issues
-		case Instr::oppop: {
-			mnemonic = fmt::format("popv.{}", type_suffix(t2));
-			params = resolve_variable_name(*(_block_ptr++));
-		} break;
-
-		case Instr::opcall: {
-			mnemonic = fmt::format("call.{}", type_suffix(t1));
-			params = resolve_function_name(*(_block_ptr++));
-		} break;
-
-		case Instr::opbreak: {
-			mnemonic = "break";
-			params = fmt::to_string(static_cast<s16>(main_block & 0xFFFF));
-		} break;
-
-		default: {
-			mnemonic = "<bad>";
-			comment = "!!! This may indicate corruption";
-		}
-
-		};
-
-		// HACK -y stuff. probably refactor that part?
-		bool mnemonic_warning = !mnemonic.empty() && mnemonic[0] == '<';
-		bool params_warning = params.size() >= 3 && params[0] == '<';
-
-		comment.insert(0, fmt::format(
-			"${:08x} ",
-			fmt::join(std::vector<Block>(main_block_ptr, _block_ptr), "'")
-		));
-
-		if (mnemonic_warning)
-		{
-			fmt::print(fmt::color::brown, "{:15}", mnemonic);
-		}
-		else
-		{
-			fmt::print("{:15}", mnemonic);
-		}
-
-		if (params_warning)
-		{
-			fmt::print(fmt::color::brown, "{:20}", params);
-		}
-		else
-		{
-			fmt::print("{:20}", params);
-		}
-
-		fmt::print(fmt::color::gray, "; {}\n", comment);
+		block = disasm.end_block;
 	}
 }
--- bytecode/disasm.hpp
+++ bytecode/disasm.hpp
@@ -3,11 +3,17 @@
 #include "../unpack/decode.hpp"
 #include <vector>
 
+struct DisassembledInstruction
+{
+	std::string location, mnemonic = "<unknown>", params, comment;
+	const Block *begin_block, *end_block;
+
+	std::string as_plain_string() const;
+};
+
 class Disassembler
 {
 	const Form& _form;
-
-	const Block* _block_ptr;
 
 public:
 	Disassembler(const Form& form) :
@@ -21,6 +27,8 @@ public:
 	std::string get_string(s32 id);
 	std::string resolve_variable_name(s32 id);
 	std::string resolve_function_name(s32 id);
+
+	DisassembledInstruction disassemble_block(const Block* block, const Script* script = nullptr);
 
 	void operator()(const Script& script);
 };
