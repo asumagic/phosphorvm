e45c1c93d17af85789e20d0b244fdbbab6d1af03
AsuMagic <sdelang@sdelang.fr>
2018-11-23 14:38:34 +0100

    Change test bench to new factorial bench

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main()
 			Disassembler{main_form}(script);
 		}
 
-		if (script.name == "gml_Script_script_fibo")
+		/*if (script.name == "gml_Script_script_fibo")
 		{
 			VM vm{main_form};
 			vm.push_stack_variable(s32(35));
@@ -33,9 +33,9 @@ int main()
 
 			fmt::print("FINAL STACK: \n");
 			vm.print_stack_frame();
-		}
+		}*/
 
-		if (script.name == "gml_Object_object0_Create_0")
+		if (script.name == "gml_Object_object1_Create_0")
 		{
 			VM vm{main_form};
 
