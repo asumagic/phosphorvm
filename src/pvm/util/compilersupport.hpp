da93d66bece627e8d7acff48b880d56ee4318166
AsuMagic <sdelang@sdelang.fr>
2018-11-26 23:42:34 +0100

    Overhaul maybe_unreachable, depend on compilersupport.hpp

--- src/pvm/util/compilersupport.hpp
+++ src/pvm/util/compilersupport.hpp
@@ -2,6 +2,8 @@
 
 #ifdef __GNUC__
     #define FORCE_INLINE __attribute__((always_inline))
+	#define UNREACHABLE __builtin_unreachable();
 #else
     #define FORCE_INLINE
+	#define UNREACHABLE
 #endif
--- src/pvm/util/errormanagement.hpp
+++ src/pvm/util/errormanagement.hpp
@@ -2,18 +2,16 @@
 
 #include <stdexcept>
 #include "pvm/config.hpp"
+#include "pvm/util/compilersupport.hpp"
 
 //! Call to designate a normally unreachable spot.
-//! With debug_mode set, throws an exception. Otherwise, uses
-//! __builtin_unreachble (effectively becoming an optimization).
-[[noreturn, gnu::cold, gnu::always_inline]]
-inline void maybe_unreachable(
-	const char* error = "VM state should never be reached"
-) {
+//! With debug_mode set, throws an exception, otherwise purposefully UB.
+FORCE_INLINE
+inline void maybe_unreachable(const char* error = "Unreachable VM state") {
 	if constexpr (check(debug::vm_safer))
 	{
 		throw std::runtime_error{error};
 	}
 
-	__builtin_unreachable();
+	UNREACHABLE
 }
